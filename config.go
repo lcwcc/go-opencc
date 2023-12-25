6f7c6ada19fc37e6a24864d13d205735f99dc858
15572669925 <2942266375@qq.com>
2023-12-25 18:39:22 +0800

    初始化

---
+++ config.go
@@ -0,0 +1,165 @@
+package go_opencc
+
+import (
+	"bufio"
+	"os"
+	"path"
+	"strings"
+)
+
+const (
+	TYPE_OCD   = "ocd"
+	TYPE_GROUP = "group"
+)
+
+var dataDir string
+
+type FileOCD string
+
+type Dict struct {
+	Type   string  `json:"type"`
+	File   FileOCD `json:"file"`
+	Dicts  []*Dict `json:"dicts"`
+	CfgMap map[string][]string
+	maxLen int //最大长度
+	minLen int //最小长度
+}
+
+type Segmentation struct {
+	Type string `json:"type"`
+	Dict *Dict  `json:"dict"`
+}
+
+type ConversionChain struct {
+	Dict *Dict `json:"dict"`
+}
+
+type Config struct {
+	Name            string             `json:"name"`
+	Segmentation    Segmentation       `json:"segmentation"`
+	ConversionChain []*ConversionChain `json:"conversion_chain"`
+}
+
+func (c *Config) init() error {
+	var err error
+	for _, cv := range c.ConversionChain {
+		err = cv.init()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (cv *ConversionChain) init() error {
+	return cv.Dict.init()
+}
+
+func (d *Dict) init() (err error) {
+	if len(d.File) > 0 {
+		d.CfgMap, d.maxLen, d.minLen, err = d.File.readFile()
+		//fmt.Println("File = ", string(d.File), d.maxLen, d.minLen)
+		if err != nil {
+			return err
+		}
+	}
+	//
+	if d.Dicts != nil && len(d.Dicts) > 0 {
+		for _, childDict := range d.Dicts {
+			err = childDict.init()
+			if err != nil {
+				return
+			}
+		}
+	}
+	return nil
+}
+
+func (fo *FileOCD) readFile() (map[string][]string, int, int, error) {
+	fileName := path.Join("data/dictionary", string(*fo))
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, 0, 0, err
+	}
+	cfgMap := make(map[string][]string)
+	buf := bufio.NewReader(f)
+	//
+	max := 0
+	min := 0
+	//
+	for {
+		line, err := buf.ReadString('\n')
+		if err != nil {
+			return cfgMap, max, min, nil
+		}
+		fields := strings.Fields(line)
+		if len(fields) > 1 {
+			if len([]rune(fields[0])) > max {
+				max = len([]rune(fields[0]))
+			}
+			if min <= 0 || len([]rune(fields[0])) < min {
+				min = len([]rune(fields[0]))
+			}
+			cfgMap[fields[0]] = fields[1:]
+		}
+	}
+	return cfgMap, max, min, nil
+}
+
+// =============================================================
+func (c *Config) convertText(text string) (string, error) {
+	var err error
+	for _, cv := range c.ConversionChain {
+		text, err = cv.convertText(text)
+		if err != nil {
+			return text, err
+		}
+	}
+	return text, nil
+}
+func (c *ConversionChain) convertText(text string) (string, error) {
+	var err error
+	text, err = c.Dict.convertTextWithMap(text)
+	if err != nil {
+		return text, err
+	}
+	return text, nil
+}
+
+func (d *Dict) convertTextWithMap(text string) (string, error) {
+	var err error
+	newText := text
+	runes := []rune(text)
+	//
+	if d.CfgMap != nil {
+		if len(runes) < d.minLen {
+			return text, nil
+		}
+		//
+		maxL := d.maxLen
+		if maxL > len(runes) {
+			maxL = len(runes)
+		}
+		//
+		for i := maxL; i >= d.minLen; i-- {
+			for j := 0; j <= len(runes)-i; j++ {
+				if i == 0 || j+i > len(runes) {
+					continue
+				}
+				old := string(runes[j : j+i])
+				if newStr, ok := d.CfgMap[old]; ok {
+					newText = strings.Replace(newText, old, newStr[0], 1)
+					j = j + i - 1
+				}
+			}
+		}
+	}
+	//
+	for _, cd := range d.Dicts {
+		newText, err = cd.convertTextWithMap(newText)
+		if err != nil {
+			return text, err
+		}
+
+	}
+	return newText, nil
+}
---
+++ config_test.go
@@ -0,0 +1,71 @@
+package go_opencc
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func Test_config(t *testing.T) {
+	fileName := `s2t.json`
+	body, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var conf *Config
+	err = json.Unmarshal(body, &conf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(conf)
+
+}
+
+func Test_opencc(t *testing.T) {
+	cc, err := NewOpenCC("s2twp")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	nText, err := cc.ConvertText(`保税工厂声明：本书为无限小说网(txt53.com)以下作品内容之版权与本站无任何关系`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(nText)
+
+}
+func Test_openccFile(t *testing.T) {
+	localdir := filepath.Dir(os.Args[0])
+
+	inFile, err := os.Open(localdir + "/神剑渡魔.txt")
+	if err != nil {
+		fmt.Println("in:", err)
+		return
+	}
+	outFile, err := os.OpenFile(localdir+"/神剑渡魔_3.txt", os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("out:", err)
+		return
+	}
+	cc, err := NewOpenCC("s2t")
+	if err != nil {
+		fmt.Println("cc:", err)
+		return
+	}
+	startTime := time.Now()
+	log.Println("start...")
+	err = cc.ConvertFile(inFile, outFile)
+	if err != nil {
+		fmt.Println("ccf", err)
+		return
+	}
+	log.Println("end...", time.Now().Unix()-startTime.Unix())
+}
